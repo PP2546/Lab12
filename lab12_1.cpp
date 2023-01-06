9ec12570c4d24f37ae3c71fe2e8f03f308b0d1f6
PP <phiphatphong_ka@cmu.ac.th>
2023-01-06 17:09:38 +0700

    Full

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,2 +1,41 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+int main (){
+    cout << "Press Enter 3 times to reveal your future.";
+    string G;
+    srand(time(0));
+    int x=rand()%9;
+        if(x == 0){ 
+        G = "A";
+        }else if(x == 1){
+        G = "B+";
+        }else if(x == 2){
+        G = "B";
+        }else if(x == 3){
+        G = "C+";
+        }else if(x == 4){
+        G = "C";
+        }else if(x == 5){
+        G = "D+";
+        }else if(x == 6){
+        G = "D";
+        }else if(x == 7){
+        G = "F";
+        }else{
+        G = "W";
+        }
+    cin.get();
+    cin.get();
+    cin.get();
+    cout << "You will get " << G << " in this 261102.";
+    return 0;
+}
+
+
+
+
